1636234dc2712343e3c2300761f40b5934bfe57d
Rameez Rahman | Kramp <Rameez.Rahman@kramp.com>
2017-01-21 13:40:05 +0100

    Addition

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("I am adding something");
-		}
+		int a = 9;
+		int b = 8;
+		System.out.println(a+b);		}
 
 }
